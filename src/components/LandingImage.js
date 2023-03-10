62ee76d600b6ede13a094e32bd5d8e7f7322816a
Annie Tran <88398285+abt14@users.noreply.github.com>
2023-03-09 18:03:56 -0800

    Landing Page

---
+++ src/components/LandingImage.js
@@ -0,0 +1,54 @@
+import React, { useState, useRef, useCallback } from "react";
+import LandingGif from "./LandingGif";
+import styled from "styled-components";
+import { mediaQueries } from "../shared/config";
+
+const Credits = styled.div`
+  float: right;
+  color: white;
+  margin-right: 10px;
+  padding-top: 0.5em;
+  font-size: 18px;
+  font-family: Sofia Sans;
+  ${mediaQueries.mobile} {
+    font-size: 14px;
+  }
+`;
+
+const LandingImage = (props) => {
+  let landingImage = props.landing_image;
+  let landingCredits = props.landing_credits;
+  let landingStatic = props.landing_image_static;
+
+  let tempLanding =
+    "https://user-images.githubusercontent.com/36126914/154766367-2c3d9c80-3cdc-4790-b15c-7eba5eee9fd2.gif";
+
+  let tempCredits = "(Credits: Joe Bruin)";
+
+  const gifEndRef = useRef(null);
+  const [firstTime, setFirstTime] = useState(true);
+
+  const scroller = useCallback(() => {
+    setTimeout(() => {
+      if (gifEndRef && gifEndRef.current) {
+        gifEndRef.current.scrollIntoView({ behavior: "smooth" });
+        setFirstTime(false);
+      }
+    }, 4000);
+  }, [gifEndRef]);
+
+  return (
+    <>
+      {firstTime ? (
+        <LandingGif landing_image={landingImage} />
+      ) : (
+        <LandingGif landing_image={landingStatic} />
+      )}
+      <div ref={gifEndRef}></div>
+      {firstTime ? scroller() : null}
+      <Credits>{landingCredits}</Credits>
+    </>
+  );
+};
+
+export default LandingImage;
