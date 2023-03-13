9a36ea788548219ba7c5996a8e47e230f989c25c
Donovan Rimer <114881191+donnyr5@users.noreply.github.com>
2023-03-13 14:47:51 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,34 @@
 import styled from 'styled-components'
-
 import DBLogo from '../images/DailyBruinLogo.svg'
 import { colors } from '../shared/config'
 
 const DBHeader = styled("div")`
   z-index: 2001;
 
-  position: -webkit-sticky;
-  position: sticky;
+  position: relative;
   top: 0;
-  background: black;
+  background: #023047;
   width: 100%;
   padding: 0.2em 0;
-  color: white;
+  text-color: white;
   font-family: 'ITC Century';
   font-style: normal;
   font-weight: 400;
   text-align: center;
   text-transform: uppercase;
   font-size: 18px;
-  line-height: 21.6px;
+  line-height: 22 px;
   /* border-bottom: 2px solid black; */
 `;
+
 
 const Header = () => {
   return (
     <DBHeader>
       {/* Daily Bruin */}
-      <a href="https://dailybruin.com">
-        <img src={DBLogo} alt="Daily Bruin" />
+      <a href="https://dailybruin.com"> <img src={DBLogo} alt="Daily Bruin" /> 
       </a>
     </DBHeader>
   )
 }
-
 export default Header;
